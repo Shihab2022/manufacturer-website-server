0d5588e37701fe2c7211dcd6af1552190d969eef
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-24 11:58:50 +0600

    make user collection and create access token

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 let port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hfg16.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,8 +35,31 @@ async function run() {
       const toolsCollection = client.db("allegro-server").collection("product");
       const reviewCollection = client.db("allegro-server").collection("review");
       const orderCollection = client.db("allegro-server").collection("order");
+      const userCollection = client.db("allegro-server").collection("user");
 
 
+// login user and generate token
+
+      app.put("/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign(
+          { email: email },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "1h" }
+        );
+        res.send({ result, accessToken: token });
+      });
+
+
+
+      
     app.get('/tools',async(req,res)=>{
         const tools=await toolsCollection.find({}).toArray()
         res.send(tools)
@@ -56,6 +93,7 @@ async function run() {
     const cursor = await  orderCollection.deleteOne(filter);
     // console.log(id,filter)
     res.send(cursor)
+
   })
   
     } finally {
