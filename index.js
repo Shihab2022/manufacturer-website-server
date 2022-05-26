2b30d62e279ae7fe0c621c7bdf485216fa646b47
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-26 08:50:37 +0600

    amke api for payment method

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 let port = process.env.PORT || 5000;
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
@@ -52,6 +52,24 @@ async function run() {
             res.status(403).send({message : 'forbidden access . '})
           }
     }
+
+    //payment intern
+
+app.post('/create-payment-intent',async(req,res)=>{
+  const service =req.body
+
+  const price =service.cost
+    // console.log(service,price)
+  const amount=price*100
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount : amount,
+    currency: 'usd',
+    payment_method_types:['card']
+  });
+  res.send({clientSecret: paymentIntent?.client_secret})
+
+})
+
     // login user and generate token
 
     app.put("/user/:email", async (req, res) => {
